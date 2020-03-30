019d5c592a0ad5e477530e5cf2d68acfc41f7416
LucazzP <lucashpolazzo2013@gmail.com>
2020-03-30 06:48:32 -0300

    Added license and formated files

--- lib/widgets/icon_button_responsive_widget.dart
+++ lib/widgets/icon_button_responsive_widget.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 part of '../responsive_widgets.dart';
 
 // Minimum logical pixel size of the IconButton.
